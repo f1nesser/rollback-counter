75b1c3d5929d17fe34ce8537230ec6617efa6377
f1nesser <riiw0046@colorado.edu>
2022-06-24 06:45:20 -1000

    switch

--- src/app.js
+++ src/app.js
@@ -1,25 +1,34 @@
 import 'dotenv/config'
 import { Client, MessageSelectMenu } from 'discord.js'
-import request from 'request'
+import https from 'https'
+import fs from 'fs'
 
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] })
 
 client.once('ready', () => {
-    console.log(`logged in as ${client.user.tag}`)
+	console.log(`logged in as ${client.user.tag}`)
 })
 
-client.on('messageCreate', (message) => {
-    if (message.attachments.first() === undefined) {
-        console.log('no attatchments')
-        return
-    }
+client.on('messageCreate', message => {
+	if (message.attachments.first() === undefined) {
+		console.log('no attatchments')
+		return
+	}
 
-    console.log(`hi${message.attachments.keys()}hi`)
+	console.log(`hi${message.attachments.keys()}hi`)
 
-    for (let [key, value] of message.attachments) {
-        console.log(key + ' + ' + value)
-        console.log(value.url)
-    }
+	for (let [key, value] of message.attachments) {
+		console.log(key + ' + ' + value)
+		console.log(value.url)
+		const file = fs.createWriteStream(value.name)
+		const request = https.get(value.url, response => {
+			response.pipe(file)
+			file.on('finish', () => {
+				file.close
+				console.log(`${value.name} downloaded`)
+			})
+		})
+	}
 })
 
 client.login(process.env.DISCORD_TOKEN)
