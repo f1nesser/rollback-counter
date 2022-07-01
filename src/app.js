e7f45ff66457102f1248f1673d84494b09c1da94
f1nesser <riiw0046@colorado.edu>
2022-06-30 17:24:08 -1000

    switch

--- src/app.js
+++ src/app.js
@@ -1,34 +1,38 @@
-import 'dotenv/config'
-import { Client, MessageSelectMenu } from 'discord.js'
-import https from 'https'
-import fs from 'fs'
+require('dotenv/config')
+const { Client } = require('discord.js')
+const https = require('https')
+const fs = require('fs')
+const { SlippiGame } = require('@slippi/slippi-js')
 
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] })
 
 client.once('ready', () => {
-	console.log(`logged in as ${client.user.tag}`)
+    console.log(`logged in as ${client.user.tag}`)
 })
 
-client.on('messageCreate', message => {
-	if (message.attachments.first() === undefined) {
-		console.log('no attatchments')
-		return
-	}
+client.on('messageCreate', (message) => {
+    if (message.attachments.first() === undefined) {
+        console.log('no attatchments')
+        return
+    }
 
-	console.log(`hi${message.attachments.keys()}hi`)
-
-	for (let [key, value] of message.attachments) {
-		console.log(key + ' + ' + value)
-		console.log(value.url)
-		const file = fs.createWriteStream(value.name)
-		const request = https.get(value.url, response => {
-			response.pipe(file)
-			file.on('finish', () => {
-				file.close
-				console.log(`${value.name} downloaded`)
-			})
-		})
-	}
+    for (let [key, value] of message.attachments) {
+        const url = value.url
+        const fileName = value.name
+        const writeStream = fs.createWriteStream(fileName)
+        const request = https.get(url, (response) => {
+            response.pipe(writeStream)
+            writeStream.on('finish', () => {
+                writeStream.close
+                console.log(`${fileName} downloaded`)
+                const game = new SlippiGame(fileName)
+                const { count, frames, lengths } = game.getRollbackFrames()
+                console.log('count', count)
+                // console.log('frames', frames)
+                console.log('lengths', lengths)
+            })
+        })
+    }
 })
 
 client.login(process.env.DISCORD_TOKEN)
